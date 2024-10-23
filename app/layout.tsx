469de56f3c0d135026fb1002cc26e68c3ca47bc8
Vansh Verma <vvansh739@gmail.com>
2024-10-23 19:30:56 +0530

    pushing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+//import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
-import { Section } from "lucide-react";
 
 
 export const metadata: Metadata = {
