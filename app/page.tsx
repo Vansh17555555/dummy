997ad218583e543a4307fbbd7cd19bcb0733c145
Vansh Verma <vvansh739@gmail.com>
2024-11-19 22:16:48 +0530

    fixing eslint issue

--- app/page.tsx
+++ app/page.tsx
@@ -206,8 +206,9 @@ export default function WaitlistPage() {
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
           <div className="bg-gray-800 rounded-lg p-4 max-w-md w-[90%] sm:w-[80%] md:w-[60%]">
             <h2 className="text-2xl font-bold text-cyan-400 mb-4 text-center">Thank you!</h2>
-            <p className="text-lg text-white mb-4 text-center">You've successfully joined our waitlist!</p>
-            <p className="text-lg text-white mb-4 text-center">Head over to our groups to stay updated on our progress:</p>
+            <p className="text-lg text-white mb-6">You&apos;ve successfully joined our waitlist!</p>
+
+               <p className="text-lg text-white mb-4 text-center">Head over to our groups to stay updated on our progress:</p>
             <div className="flex flex-col gap-2">
               <a
                 href="https://discord.gg/fsWYHaXs"
